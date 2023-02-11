1a09408751b4cbab0424fe5c6f958b1ff9595b12
uzochukwuV <vic.ezealor@gmail.com>
2023-02-10 21:08:58 -0800

    btn

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,9 +12,11 @@ function Header() {
                     <h1>Atlas Apartment Services</h1>
                     <p>We help you find a suitable and condusive place to stay</p>
                 </div>
-                <div className='d-flex gap-3 justify-'>
+                <div className='d-flex gap-3 justify-content-center'>
+                    <span className='d-flex gap-3 justify-content-center'>
                     <button type='submit' className=' btn-3 p-3'>Sign up for Free</button>
                     <button type='submit' className=' btn bg-transparent p-3 border border-white' style={{borderRadius: "100px"}}>Contact Sales</button>
+                    </span>
                 </div>
             </div>
         </header>
