6602fdc7eea2aa4fbdc2bb295e7de846445af200
uzochukwuV <vic.ezealor@gmail.com>
2023-02-22 05:45:55 -0800

    romoved unused folders

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from './routes/home/home'
 import Login from './routes/authLogin/login'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 
 function App() {
